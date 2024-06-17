56695a74cc22411edc9030d0d7c20700a7010a0d
Abbos <norqulovabbos1701@gmail.com>
2024-06-17 22:38:01 +0500

    red

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,37 +1,37 @@
   
-//  import React from 'react'
-//   const Books = () => {
-//     return (
-//       <div>
-//           <pre>
-//               <p class="text ">
-                 
-//                 <h1>{`
-//       salom qalaysiz
-                
-//                 `}</h1>
-//                 </p>
-//             </pre>   
-    
-//         </div>
-//        )
-//      }
-    
-//      export default Books
-
-
-
  import React from 'react'
- import Img from "../assests/kursdoshlar.png"
   const Books = () => {
     return (
       <div>
-         <img className='bg-no-repeat bg-cover bg-center' src={Img} alt="kursdoshlar" />
+          <pre>
+              <p class="text ">
+                 
+                <h1>{`
+      salom qalaysiz Adiba
+                
+                `}</h1>
+                </p>
+            </pre>   
+    
         </div>
        )
      }
     
      export default Books
+
+
+
+//  import React from 'react'
+//  import Img from "../assests/kursdoshlar.png"
+//   const Books = () => {
+//     return (
+//       <div>
+//          <img className='bg-no-repeat bg-cover bg-center' src={Img} alt="kursdoshlar" />
+//         </div>
+//        )
+//      }
+    
+//      export default Books
 
   
 
